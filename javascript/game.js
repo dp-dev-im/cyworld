0a8a0d4332fc2099a650d5b985e534e72a1bf949
dominopiece <dp.dev.im@gmail.com>
2023-06-03 22:17:07 +0900

    :art: Jukebox

--- javascript/game.js
+++ javascript/game.js
@@ -1,34 +1,41 @@
+let isStarted = false;
+
 const onClick = () => {
-  const token = String(Math.round(Math.random() * 1000000)).padStart(6, "0");
-  let time = 179;
-  setInterval(function () {
-    if (time >= 0) {
-      const min = Math.floor(time / 60);
-      const sec = String(Math.floor(time % 60)).padStart(2, "0");
-      document.getElementById("timer").innerText = `${min}min ${sec}sec`;
-      //   console.log(`${min}min ${sec}sec`);
-      time--;
-    } else {
-      document.getElementById("disabled").disabled = true;
-    }
-  }, 1000);
-  document.getElementById("randomNumber").innerText = token;
-  document.getElementById("randomNumber").style.backgroundColor = `#${token}`;
-};
+  if (isStarted === false) {
+    isStarted = true
+    const token = String(Math.round(Math.random() * 1000000)).padStart(6, "0");
+    let time = 179;
+    setInterval(function () {
+      if (time >= 0) {
+        const min = Math.floor(time / 60);
+        const sec = String(Math.floor(time % 60)).padStart(2, "0");
+        document.getElementById("timer").innerText = `${min}min ${sec}sec`;
+        //   console.log(`${min}min ${sec}sec`);
+        time--;
+      } else {
+        document.getElementById("disabled").disabled = true;
+        isStarted = false
+      }
+    }, 1000);
 
-const startWord = () => {
-  let word = document.getElementById("word").innerText;
-  let inputWord = document.getElementById("inputWord").value;
-  let lastWord = word[word.length - 1];
-  // console.log(lastWord)
-  let firstWord = inputWord[0];
-
-  if (lastWord === firstWord) {
-    console.log("right");
-    document.getElementById("result").innerText = "Right";
-    document.getElementById("word").innerText = inputWord;
-    document.getElementById("inputWord").value = "";
-  } else {
-    document.getElementById("result").innerText = "Wrong";
+    document.getElementById("randomNumber").innerText = token;
+    document.getElementById("randomNumber").style.backgroundColor = `#${token}`;
   }
-};
+
+  const startWord = () => {
+    let word = document.getElementById("word").innerText;
+    let inputWord = document.getElementById("inputWord").value;
+    let lastWord = word[word.length - 1];
+    // console.log(lastWord)
+    let firstWord = inputWord[0];
+
+    if (lastWord === firstWord) {
+      console.log("right");
+      document.getElementById("result").innerText = "Right";
+      document.getElementById("word").innerText = inputWord;
+      document.getElementById("inputWord").value = "";
+    } else {
+      document.getElementById("result").innerText = "Wrong";
+    }
+  }
+}
